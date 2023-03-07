0e981d8e4b5d4be09364d0368f8bc9b7821fc135
BAravind85 <arubrp09@gmail.com>
2023-03-07 17:04:41 +0530

    loginadded

--- loginform/pages/login/index.js
+++ loginform/pages/login/index.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
-// import { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useRouter } from "next/router";
-import axios from "axios";  
+import axios from "axios";
 import { toast } from "react-toastify";
+import Image from "next/image";
+import eye_image from "../home/eye.png";
+import eye_slash from "../home/eye_slash.png";
 
 export default function Login() {
+  const [eye, seteye] = useState(true);
+  const [password, setpassword] = useState("password");
+
   const router = useRouter();
   const validationSchema = yup.object().shape({
     email: yup
@@ -27,28 +33,38 @@ export default function Login() {
     handleSubmit,
   } = useForm(formOptions);
   const onSubmit = async (data) => {
-    try{
-    const result = await axios.post("http://localhost:3001/api/login", data)
-    console.log("result", result);
-    console.log(data);
-    toast("Login Successfully", {
-      hideProgressBar: true,
-      autoClose: 2000,
-      type: "success",
-      position: "top-right",
-    });
-    router.push("/home");
-  }catch(error){
-    console.log(error)
-    toast("Email already in user or Password",{
-      hideProgressBar: true,
-      autoClose: 2000,
-      type: "error",
-      position: "top-center",
-    });
-  }
+    try {
+      const result = await axios.post("http://localhost:3001/api/login", data);
+      console.log("result", result);
+      console.log(data);
+      toast("Login Successfully", {
+        hideProgressBar: true,
+        autoClose: 2000,
+        type: "success",
+        position: "top-right",
+      });
+      router.push("/home");
+    } catch (error) {
+      console.log(error);
+      toast("Email already in user or Password", {
+        hideProgressBar: true,
+        autoClose: 2000,
+        type: "error",
+        position: "top-center",
+      });
+    }
   };
   console.log(errors);
+
+  const Eye = () => {
+    if (password === "password") {
+      setpassword("text");
+      seteye(false);
+    } else {
+      setpassword("password");
+      seteye(true);
+    }
+  };
   // const [state, setState] = useState({
   //   email: "",
   //   password: "",
@@ -62,54 +78,97 @@ export default function Login() {
   // };
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex justify-center items-center w-full bg-lime-50 ">
-          <div className="p-12 m-20 bg-cyan-500  h-96 shadow-2xl rounded-br-xl border-stone-300 rounded-md hover:drop-shadow-2xl">
-            <div className="text-center">
-              <div className="text-xl font-bold underline text-white">
-                Login
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex justify-center items-center w-full bg-teal-50"
+      >
+        <div className="flex pr-20">
+          <div className="m-20">
+            <img
+              className="pr-20"
+              src="https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/image_1630190079464.png"
+              alt="image"
+            />
+          </div>
+          <div className="p-15 pt-14 ">
+            <div className="mt-10 mb-4">
+              <img
+                className="object-cover h-20 w-25"
+                src="https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/logo_1630189049088.png"
+                alt="header"
+              />
+            </div>
+            <div className="p-6  w-full border-solid border-2 border-grey-600 rounded bg-white ">
+              <div className="">
+                <div className="text-2xl mb-4">Welcome Back!</div>
+                <div className="text-sm">
+                  <p className="text-neutral-500 mb-4">
+                    Login to your dashboard with your username and password.
+                  </p>
+                </div>
+              </div>
+              <div className=" ">
+                <input
+                  {...register("email", { required: true })}
+                  type="email"
+                  placeholder="Your Email"
+                  className={`mt-4 px-3 py-3 bg-white border border-slate-300 placeholder-slate-600 focus:outline-none ${
+                    errors.email ? "border-red-500 ring-red-500" : ""
+                  } block w-full rounded focus:border-indigo-600 ring-indigo-600  sm:text-sm focus:ring-1`}
+                />
+              </div>
+              {<p class="text-red-600 text-sm">{errors.email?.message}</p>}
+              <div className="flex">
+                <input
+                  type={password}
+                  {...register("password", { required: true })}
+                  placeholder="Password"
+                  className={`mt-4 px-3 py-3 bg-white border border-slate-300 placeholder-slate-600 focus:outline-none ${
+                    errors.password ? "border-red-500 ring-red-500" : ""
+                  } block w-full rounded-l-lg  focus:border-indigo-600 ring-indigo-600  sm:text-sm focus:ring-1`}
+                />
+                <div className="flex justify-center items-center w-12 bg-orange-600 mt-4 rounded-r-lg  hover:bg-orange-700">
+                  {eye ? (
+                    <Image
+                      className="object-cover w-6 "
+                      onClick={Eye}
+                      src={eye_slash}
+                      alt="eye_image"
+                    />
+                  ) : (
+                    <Image
+                      className="object-cover w-6 "
+                      onClick={Eye}
+                      src={eye_image}
+                      alt="eye_image"
+                    />
+                  )}
+                </div>
+              </div>
+              {
+                <p className="text-red-600 text-sm">
+                  {errors.password?.message}
+                </p>
+              }
+
+              <p className="text-sm mt-5 text-indigo-800">Forgot Password?</p>
+              <div className="mt-9">
+                <button
+                  className="bg-orange-500 px-3 py-3 w-full rounded text-white hover:bg-orange-600"
+                  type="submit"
+                >
+                  Login
+                </button>
               </div>
             </div>
-            <div className="pb-2">
-              <div>
-                <div className="p-1 text-base ">Email address</div>
-              </div>
-              <input
-                type="email"
-                {...register("email", { required: true })}
-                // value={state.email}
-                // onChange={(e) => setState({ ...state, email: e.target.value })}
-                placeholder="Email address"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              />
-              <p className="text-rose-700">{errors.email?.message}</p>
-            </div>
-            <div className="space-y-6">Password</div>
-            <div>
-              <input
-                type="password"
-                {...register("password", { required: true })}
-                // value={state.password}
-                // onChange={(e) => setState({ ...state, password: e.target.value })}
-                placeholder="Password"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              />
-              <p className="text-rose-700">{errors.password?.message}</p>
-            </div>
-            <div className="text-center p-2">
-              <button
-                type="submit"
-                className="h-full w-20 p-1 bg-white rounded-md hover:bg-sky-200"
-                // onClick={onSubmit}
+            <div className="flex justify-center  mt-4 ">
+              Don't have an account ? &nbsp;
+              <Link
+                href="/register"
+                className="text-blue-600 hover:text-indigo-700"
               >
-                SignIn
-              </button>
-            </div>
-            <div>
-              You don't have an account ?
-              <Link href="/register" className="text-blue-600 hover:text-white">
                 {" "}
-                Register here
+                Register now!
               </Link>
             </div>
           </div>
--- loginform/pages/register/index.js
+++ loginform/pages/register/index.js
@@ -1,14 +1,26 @@
 import Link from "next/link";
-// import { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import eye_image from "../home/eye.png";
+import eye_slash from "../home/eye_slash.png";
 
 // import { useEffect } from "react";
 export default function Home() {
+  const [inputtext, setinputtext] = useState({
+    email: "",
+    password: "",
+  });
+  const [eye, seteye] = useState(true);
+  const [password, setpassword] = useState("password");
+  const [type, settype] = useState(false);
+
+  //
   const router = useRouter();
   const validationSchema = yup.object().shape({
     email: yup
@@ -35,28 +47,38 @@ export default function Home() {
     handleSubmit,
   } = useForm(formOptions);
   const onSubmit = async (data) => {
-    try{
-    const result = await axios.post("http://localhost:3001/api/signup", data)
-    console.log("result", result);
-    console.log(data);
-    toast("Registared Successfully", {
-      hideProgressBar: true,
-      autoClose: 2000,
-      type: "success",
-      position: "top-right",
-    });
-    router.push("/login");
-  }catch(error){
-    console.log(error)
-    toast("Email already in user or Password",{
-      hideProgressBar: true,
-      autoClose: 2000,
-      type: "error",
-      position: "top-right",
-    });
-  }
+    try {
+      const result = await axios.post("http://localhost:3001/api/signup", data);
+      console.log("result", result);
+      console.log(data);
+      toast("Registared Successfully", {
+        hideProgressBar: true,
+        autoClose: 2000,
+        type: "success",
+        position: "top-right",
+      });
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+      toast("Email already in user or Password", {
+        hideProgressBar: true,
+        autoClose: 2000,
+        type: "error",
+        position: "top-right",
+      });
+    }
   };
   console.log(errors);
+
+  const Eye = () => {
+    if (password === "password") {
+      setpassword("text");
+      seteye(false);
+    } else {
+      setpassword("password");
+      seteye(true);
+    }
+  };
 
   // const [state, setState] = useState({
   //   email: "",
@@ -71,86 +93,113 @@ export default function Home() {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex justify-center items-center w-full bg-teal-50">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex justify-center items-center w-full bg-teal-50"
+      >
         <div className="flex pr-20">
           <div className="m-20">
-          <img className="pr-20"src="https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/image_1630190079464.png" alt="image"/>
+            <img
+              className="pr-20"
+              src="https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/image_1630190079464.png"
+              alt="image"
+            />
           </div>
-          <div className='p-15'>
+          <div className="p-15 pt-10">
             <div className="mt-10 mb-4">
-            <img className="object-cover h-20 w-25"src="https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/logo_1630189049088.png" alt="header"/>
-            </div>     
-          <div className="p-6  w-96 border-solid border-2 border-grey-600 rounded bg-white ">
-            <div className="">
-              <div className="text-2xl mb-4">
-                Register Account
+              <img
+                className="object-cover h-20 w-25"
+                src="https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/logo_1630189049088.png"
+                alt="header"
+              />
+            </div>
+            <div className="p-6  w-96 border-solid border-2 border-grey-600 rounded bg-white ">
+              <div className="">
+                <div className="text-2xl mb-4">Register Account</div>
+                <div className="text-sm">
+                  <p className="text-neutral-500 mb-4">
+                    Free on-boarding support. No credit card required.
+                  </p>
+                </div>
               </div>
-              <div className="text-sm">
-                  <p className="text-neutral-500 mb-4">Free on-boarding support. No credit card required.</p>
+              <div className=" ">
+                <input
+                  {...register("email", { required: true })}
+                  type="email"
+                  placeholder="Your Email"
+                  className={`mt-4 px-3 py-3 bg-white border border-slate-300 placeholder-slate-600 focus:outline-none ${
+                    errors.email ? "border-red-500 ring-red-500" : ""
+                  } block w-full rounded focus:border-indigo-600 ring-indigo-600  sm:text-sm focus:ring-1`}
+                />
               </div>
-            </div>
-            <div className="pb-2 ">
-              <input
-                {...register("email", { required: true })}
-                type="email"
-                // value={state.email}
-                // onChange={(e) => setState({ ...state, email: e.target.value })}
-                placeholder="Your Email"            
-                className="mt-1 px-3 py-3 bg-white border shadow-sm border-slate-300 placeholder-slate-600 focus:outline-none focus:border-indigo-700 focus:ring-indigo-700 block w-full rounded  sm:text-sm focus:ring-1 
-                disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-                  invalid:border-pink-500 invalid:text-pink-600
-                  focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
-              />
-            </div>
-              {<p className="text-rose-700">{errors.email?.message}</p>}
-            <div>
-              <input
-                type="password" // if show password is true type will be text otherwise its text
-                // value={state.password}
-                // onChange={(e) =>
-                //   setState({ ...state, password: e.target.value })
-                // }
-                {...register("password", { required: true })}
-                placeholder="Password"
-                className="mt-4 px-3 py-3 bg-white border border-slate-300 placeholder-slate-600 focus:outline-none focus:border-indigo-700 focus:ring-indigo-700 block w-full rounded  sm:text-sm focus:ring-1"
-              />
-               {<p className="text-rose-700">{errors.password?.message}</p>}
-              <img />
-            </div>
-            <div>
-              <input
-                type="password"
-                // value={state.confirmPassword}
-                // onChange={(e) =>
-                //   setState({ ...state, confirmPassword: e.target.value })
-                // }
-                {...register("confirmPassword", { required: true })}
-                placeholder="Confrim Password"
-                className="mt-5 px-3 py-3 bg-white  border shadow-sm border-slate-300 placeholder-slate-600 focus:outline-none focus:border-indigo-700 focus:ring-indigo-700 block w-full rounded sm:text-sm focus:ring-1"
-              />
-              <p className="text-rose-700">{errors.confirmPassword?.message}</p>
-            </div>
-            <div className="mt-9">
-              <button
-                className="bg-orange-600 px-3 py-3 w-full rounded text-white hover:bg-orange-700"
-                type="submit"
-              >
-                SignUp
-              </button>
-            </div>
+              {<p class="text-red-600 text-sm">{errors.email?.message}</p>}
+              <div className="flex">
+                <input
+                  type={password}
+                  {...register("password", { required: true })}
+                  placeholder="Password"
+                  className={`mt-4 px-3 py-3 bg-white border border-slate-300 placeholder-slate-600 focus:outline-none ${
+                    errors.password ? "border-red-500 ring-red-500" : ""
+                  } block w-full rounded-l-lg  focus:border-indigo-600 ring-indigo-600  sm:text-sm focus:ring-1`}
+                />
+                <div className="flex justify-center items-center w-12 bg-orange-600 mt-4 rounded-r-lg  hover:bg-orange-700">
+                  {eye ? (
+                    <Image
+                      className="object-cover w-6 "
+                      onClick={Eye}
+                      src={eye_slash}
+                      alt="eye_image"
+                    />
+                  ) : (
+                    <Image
+                      className="object-cover w-6 "
+                      onClick={Eye}
+                      src={eye_image}
+                      alt="eye_image"
+                    />
+                  )}
+                </div>
+              </div>
+              {
+                <p className="text-red-600 text-sm">
+                  {errors.password?.message}
+                </p>
+              }
+              <div>
+                <input
+                  type={password}
+                  {...register("confirmPassword", { required: true })}
+                  placeholder="Confrim Password"
+                  className={`mt-4 px-3 py-3 bg-white border border-slate-300 placeholder-slate-600 focus:outline-none ${
+                    errors.password ? "border-red-500 ring-red-500" : ""
+                  } block w-full rounded  focus:border-indigo-600 ring-indigo-600  sm:text-sm focus:ring-1`}
+                />
+                <p className="text-red-600 text-sm">
+                  {errors.confirmPassword?.message}
+                </p>
+              </div>
+              <div className="mt-9">
+                <button
+                  className="bg-orange-500 px-3 py-3 w-full rounded text-white hover:bg-orange-600"
+                  type="submit"
+                >
+                  SignUp
+                </button>
+              </div>
             </div>
             <div className="flex justify-center  mt-4 ">
-              Already have an account ?
-              <Link href="/login" className="text-blue-600 hover:text-indigo-700">
+              Already have an account ? &nbsp;
+              <Link
+                href="/login"
+                className="text-blue-600 hover:text-indigo-700"
+              >
                 {" "}
                 Login here
               </Link>
             </div>
-            </div>
+          </div>
         </div>
       </form>
     </>
   );
 }
-//https://posbytz.s3.ap-south-1.amazonaws.com/partners/1/image_1630190079464.png
-{/* <div class="bg-no-repeat bg-left ..." style="background-image: url(...);"></div> */}
